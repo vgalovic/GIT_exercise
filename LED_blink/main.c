7bc3e42e3445f2c17d96fb779c82b8cc8ca008fe
Vladimir Galovic <vgalovic99@gmail.com>
2022-03-19 16:42:19 +0100

    ledBlink function added

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -1,11 +1,22 @@
 # include < avr / io .h >
 # include < util / delay .h >
 # include < stdint .h >
-void ledBlink(int16_t
-high_time , int16_t
-low_time)
-{
-PORTB |= 1 << 5; // LED ON
-_delay_ms(high_time); // Pauza 1s
-PORTB &= ~(1 << 5); // LED OFF
-_delay_ms(low_time); // Pauza 1s
+
+int16_t main (){
+
+    int16_t high_time = 300;
+    int16_t low_time = 700;
+    while (1){
+        ledBlink(high_time, low_time);
+    }
+
+    return 0;
+}
+
+void ledBlink(int16_t high_time , int16_t low_time){
+
+    PORTB |= 1 << 5; // LED ON
+    _delay_ms(high_time); // Pauza 1s
+    PORTB &= ~(1 << 5); // LED OFF
+_   delay_ms(low_time); // Pauza 1s
+}
