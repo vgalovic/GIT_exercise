89ed8b56216b15812dd4df4995bad8d70ba0b0f3
Vladimir Galovic <vgalovic99@gmail.com>
2022-03-19 16:29:41 +0100

    high_time and low_time values changed

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -3,8 +3,8 @@
 # include < stdint .h >
 int16_t main ()
 {
-int16_t high_time = 500;
-int16_t low_time = 500;
+int16_t high_time = 300;
+int16_t low_time = 700;
 DDRB |= 1 << 5; // PB5 je izlaz
 while (1)
 {
