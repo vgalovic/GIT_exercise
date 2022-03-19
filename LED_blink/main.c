b40f3f5c41d6b578a7cf87bf38bb3906fb4dc06d
Vladimir Galovic <vgalovic99@gmail.com>
2022-03-19 16:46:03 +0100

    ledInit function added

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -1,17 +1,24 @@
 # include < avr / io .h >
 # include < util / delay .h >
 # include < stdint .h >
-int16_t main ()
-{
-int16_t high_time = 300;
-int16_t low_time = 700;
-DDRB |= 1 << 5; // PB5 je izlaz
-while (1)
-{
-PORTB |= 1 << 5; // LED ON
-_delay_ms ( high_time ) ; // Pauza 1 s
-PORTB &= ~(1 << 5) ; // LED OFF
-_delay_ms ( low_time ) ; // Pauza 1 s
+
+int16_t main (){
+
+    int16_t high_time = 300;
+    int16_t low_time = 700;
+    ledInit();
+
+    while (1){
+
+        PORTB |= 1 << 5; // LED ON
+        _delay_ms ( high_time ) ; // Pauza 1 s
+        PORTB &= ~(1 << 5) ; // LED OFF
+        _delay_ms ( low_time ) ; // Pauza 1 s   
+    }   
+
+    return 0;
 }
-return 0;
+
+void ledInit (){
+    DDRB |= 1 << 5; //PB5 je izlaz
 }
